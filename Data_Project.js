9f2ecc00e51e5763de8d4645225979ea80baf2af
Igor Bessa <ibessa@ucsc.edu>
2024-04-25 22:03:59 -0700

    finished project

--- Data_Project.js
+++ Data_Project.js
@@ -3,7 +3,7 @@ let input;
 let imagine;
 let res = 30;
 let colors;
-let DarkBlue, LightBlue;
+let color1, color2;
 
 function handleImage(file){
   if(file.type === 'image'){
@@ -21,15 +21,28 @@ function handleImage(file){
 function setup() {
   input = createFileInput(handleImage);
   input.position(150, 300);
-
-  // establish the colors we want for our palette
-  colors = [[50, 94, 168], [115, 163, 245], [162, 185, 224], [30, 59, 107], [4, 20, 46], [0, 0, 0]];
-  // establish the range of colors for our palette 
-  DarkBlue = color(2, 12, 28);
-  LightBlue = color(215, 227, 255);
-
   // set a default size for the canvas
   createCanvas(500, 500);
+  //background(255);
+  textSize(30);
+  fill(0);
+  stroke(0);
+  strokeWeight(4);
+
+  //textAlign('CENTER');
+  let message = 'Some browsers have issues displaying the image you upload. If this happens, reload the browser and try uploading the image again';
+  text(message, 150, 0);
+
+  // establish the range of colors for our palette 
+  // ------------------------ blues -------------------------------
+  // color1 = color(2, 12, 28);
+  // color2 = color(120, 140, 255);
+  // ------------------------ greens ------------------------------
+  // color1 = color(5, 33, 2);
+  // color2 = color(209, 255, 204);
+  // ------------------------ purples -----------------------------
+  color1 = color(23, 2, 41);
+  color2 = color(225, 211, 237);
 
   // set the pixel density to 1
   pixelDensity(1);
@@ -39,12 +52,12 @@ function fixCanvas(){
   if(img){
     if (width != img.width || height != img.height){
       resizeCanvas(img.width, img.height)
-      background(255, 50, 50)
     }
   }
 }
 
 function draw() {
+  //background(255);
   if(imagine){
     image(imagine, 0, 0, width, height);
     imagine.loadPixels();
@@ -59,7 +72,7 @@ function draw() {
         
         let normalizedBright = map(bright, 0, 100, 0, 1);     // normalize the brightness
         
-        let colore = lerpColor(DarkBlue, LightBlue, normalizedBright);      // choose a color based on the normalized brightness
+        let colore = lerpColor(color1, color2, normalizedBright);      // choose a color based on the normalized brightness
         
         fill(colore);             // fill the rectangle that new color
         rect(x,y, res, res);      // draw the square using the color we found
